92171302b902695b44249fb1b2c102b25f8fcfbd
jonnaks <jonnaks@kth.se>
2016-12-08 09:23:20 +0100

    Man Standing on Business Card

---
+++ ARBusinessCard/Assets/vbscript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class vbscript : MonoBehaviour, IVirtualButtonEventHandler{
+
+	private GameObject vbButtonObject;
+	private GameObject manInSuit;
+	// Use this for initialization
+	void Start () {
+		vbButtonObject = GameObject.Find ("actionButton");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+		vbButtonObject = GameObject.Find ("manInSuit");
+		Debug.Log ("Button Pressed!!!!!");
+		 
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+	
+	}
+
+}
