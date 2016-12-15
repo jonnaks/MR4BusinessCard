7917cc46abf728b132297023ecf77ed566a8a3ff
samuelsson87 <samuelsson87@hotmail.com>
2016-12-15 14:01:54 +0100

    start

---
+++ Assets/vbscript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vbscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Handheld.PlayFullScreenMovie("Jonna2.mov");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
