ac143dbc96d05fc3ceaf5c28443718d785560e2f
jonnaks <jonnaks@kth.se>
2016-12-15 11:25:41 +0100

    LinkedInButton starting directly and are released when removing object

---
+++ Assets/vbScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vbButtonObject;
+	// Use this for initialization
+	void Start () {
+
+		vbButtonObject = GameObject.Find ("linkedInButton");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+
+		Debug.Log ("Button Pressed!!!!!!!!!");
+	}
+
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb){
+		Debug.Log ("Button Released!!!!!!!!!");
+	}
+
+	void Update () {
+
+	}
+
+}
