3a194e9e752c66fc7c6d5869b460e96c8d04916d
samuelsson87 <samuelsson87@hotmail.com>
2016-12-15 12:48:33 +0100

    1.0
    Spelar ej någon video. Inte heller originalfilen.

---
+++ Assets/vbScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public static bool PlayFullScreenMovie(string /Users/David/Desktop/KTH/Github/MR4BusinessCard/Assets/StreamingAssets, Color bgColor = Color.black, FullScreenMovieControlMode controlMode = FullScreenMovieControlMode.Full, FullScreenMovieScalingMode scalingMode = FullScreenMovieScalingMode.AspectFit);
+public class vbScript : MonoBehaviour {
+
+	private string movPath = "Jonna2.mov";
+
+	// Use this for initialization
+	void Start () {
+
+		if (Input.GetButtonDown ("Jump")) {
+			StartCoroutine (PlayStreamingVideo (movPath));  //ska spela om man trycker på mellanslag
+		}
+	}
+
+	private IEnumerator PlayStreamingVideo(string url)
+	{
+		Handheld.PlayFullScreenMovie(url, Color.black, FullScreenMovieControlMode.Full, FullScreenMovieScalingMode.AspectFill);
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
+		Debug.Log("Video playback completed.");
+	}
+}
