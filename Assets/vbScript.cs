cc718adbbde7b63b5a58eda129652d019987e04b
jonnaks <jonnaks@kth.se>
2016-12-19 11:20:39 +0100

    Still not workin

--- Assets/vbScript.cs
+++ Assets/vbScript.cs
@@ -5,23 +5,79 @@ using Vuforia;
 
 public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
 
-	private GameObject vbButtonObject;
-	// Use this for initialization
-	void Start () {
+	#region PUBLIC_METHODS
 
-		vbButtonObject = GameObject.Find ("linkedInButton");
-		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
-	}
-
-	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
-
-		Debug.Log ("Button Pressed!!!!!!!!");
+	/// <summary>
+	/// Called when the virtual button has just been pressed:
+	/// </summary>
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+	{
+		Debug.Log("OnButtonPressed for " + vb.name );
 		Application.OpenURL ("http://www.google.com");
+
 	}
 
-	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb){
-		Debug.Log ("Button Released!!!!!!!!!");
+
+	/// <summary>
+	/// Called when the virtual button has just been released:
+	/// </summary>
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+	{
+		Debug.Log("OnButtonReleased for " + vb.name );
 	}
-		
+
+	#endregion // PUBLIC_METHODS
+
+
+
+	#region UNTIY_MONOBEHAVIOUR_METHODS
+
+	void Start()
+	{
+
+		// Register with the virtual buttons TrackableBehaviour
+		VirtualButtonBehaviour vb = GetComponentInChildren<VirtualButtonBehaviour>();
+		if (vb)
+		{
+			vb.RegisterEventHandler(this);
+		}
+
+	}
+
+
+	void Update()
+	{
+
+	}
+
+	#endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+
+
+	#region PRIVATE_METHODS
+
+
+
+	#endregion // PRIVATE_METHODS
+
+
+//	private GameObject vbButtonObject;
+//	// Use this for initialization
+//	void Start () {
+//
+//		vbButtonObject = GameObject.Find ("linkedInButton");
+//		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+//	}
+//
+//	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+//
+//		Debug.Log ("Button Pressed!!!!!!!!");
+//		Application.OpenURL ("http://www.google.com");
+//	}
+//
+//	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb){
+//		Debug.Log ("Button Released!!!!!!!!!");
+//	}
+//		
 
 }
