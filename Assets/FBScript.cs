c8cfc27247cd8e3643527c4d12e7a284c56e5e85
jonnaks <jonnaks@kth.se>
2016-12-19 10:14:30 +0100

    Button still not workin

---
+++ Assets/FBScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class FBScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vbButtonObject;
+	// Use this for initialization
+	void Start () {
+
+		vbButtonObject = GameObject.Find ("FbButton");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+
+		Debug.Log ("Button DOWN");
+		Application.OpenURL ("http://www.facebook.com");
+	}
+
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb){
+		Debug.Log ("Button UP");
+	}
+}
